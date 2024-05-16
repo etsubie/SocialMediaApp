898f0e357875e154d55d5d4fe3a8f14f3ab5f859
Etsubdink Yetwale <etsubie2124@gmail.com>
2024-05-16 13:21:38 +0300

    updated

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -3,6 +3,9 @@ import postModel from "../models/postModel.js";
 export const fechPosts = async (req, res) => {
   try {
     const posts = await postModel.find();
+    if(!posts){
+      res.status(404).json("no posts found")
+    }
     res.status(200).json({count: posts.length, posts});
   } catch (error) {
     res.status(500).json({message: error.message})
@@ -14,7 +17,7 @@ export const fechPost = async(req, res) => {
         const {id} = req.params
         const post = await postModel.findById(id)
         if(!post){
-          return  res.status(404).json('not found')
+          return  res.status(404).json('post not found')
         }
         res.status(200).json(post)
     } catch (error) {
@@ -37,7 +40,10 @@ export const updatePost = async (req, res) => {
   try {
     const { id } = req.params;
     const post = req.body;
-    const updatedPost = await postModel.findByIdAndUpdate(id, post);
+    const updatedPost = await postModel.findByIdAndUpdate(id, post, {new: true});
+    if(!updatedPost){
+      return res.status(404).json("Post Not Found")
+    }
     res.status(200).json(updatedPost);
   } catch (error) {
     res.status(500).json({message: error.message})
@@ -48,6 +54,9 @@ export const deletePost = async (req, res) => {
     try {
         const {id} = req.params
         const deletedPost = await postModel.findByIdAndDelete(id)
+        if(!deletedPost){
+          return res.status(404).json("Post Not Found")
+        }
         res.status(200).json(deletedPost)
     } catch (error) {
         console.log(error)
