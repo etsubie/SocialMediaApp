51b388c31a209707604d156d38b4ff9967b79fd8
Etsubdink Yetwale <etsubie2124@gmail.com>
2024-07-26 17:27:01 +0300

    updated

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,8 @@ import postRouter from "./routes/postRoute.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.port;
-const URL = process.env.CONNECTION_URL || 5200;
+const port = process.env.PORT;
+const URL = process.env.CONNECTION_URL;
 
 app.use(express.json({limit: '50mb'}))
 app.use(cors())
